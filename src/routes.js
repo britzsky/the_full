320cb0662c6616f35da2d15ad952d81d9ae53da0
wonu <938274@naver.com>
2026-04-13 19:58:38 +0900

    route 수정

--- src/routes.js
+++ src/routes.js
@@ -352,7 +352,8 @@ const routes = [
     collapse: [
       {
         type: "collapse",
-        name: "💳 거래처 자료 입력",
+        // name: "💳 거래처 자료 입력",
+        name: "📦 매입마감",
         key: "purchaseDeadLineTally",
         //icon: <Icon fontSize="small">*</Icon>,
         route: "/purchaseDeadLineTally/purchasetally",
@@ -396,7 +397,8 @@ const routes = [
       },
       {
         type: "collapse",
-        name: "📦 매입마감",
+        // name: "📦 매입마감",
+        name: "📦 매입집계",
         key: "purchaseTally",
         //icon: <Icon fontSize="small">*</Icon>,
         route: "/purchaseTally/purchasetally",

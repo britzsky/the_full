20b3f8f16c0df034de00587b8d7e568738aed0fe
이원우 <okmijn05@naver.com>
2026-09-07 08:42:37 +0900

    권한수정

--- src/routes.js
+++ src/routes.js
@@ -351,8 +351,8 @@ const routes = [
         //icon: <Icon fontSize="small">*</Icon>,
         route: "/Operate/MenuRecipe",
         component: <Operate_7 />,
-        allowedDepartments: [0, 2, 3, 4, 5, 6, 8, 9], // 🔹 부서권한
-        allowedPositions: [0, 1, 2, 3], // 🔹 직책권한
+        allowedDepartments: [6], // 🔹 부서권한
+        allowedPositions: [0, 1], // 🔹 직책권한
         accessMode: "AND",
       },
     ],

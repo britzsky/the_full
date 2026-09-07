125b1813b9d1f3edaf374cf0f0ebc1829dc71e12
이원우 <okmijn05@naver.com>
2026-09-07 21:31:27 +0900

    출근부수정

--- src/layouts/recordsheet/index.js
+++ src/layouts/recordsheet/index.js
@@ -6011,6 +6011,26 @@ function RecordSheet() {
                   </MDTypography>
                   {mealColumnAverages.length > 0 && (
                     <table style={{ borderCollapse: "collapse", fontSize: 11 }}>
+                      <thead>
+                        <tr>
+                          {mealColumnAverages.map((item) => (
+                            <th
+                              key={item.key}
+                              style={{
+                                border: "1px solid #ddd",
+                                padding: "2px 8px",
+                                background: "#288ebe",
+                                textAlign: "center",
+                                color: "#fff",
+                                fontWeight: 700,
+                                whiteSpace: "nowrap",
+                              }}
+                            >
+                              {item.label}
+                            </th>
+                          ))}
+                        </tr>
+                      </thead>
                       <tbody>
                         <tr>
                           {mealColumnAverages.map((item) => (
@@ -6025,7 +6045,6 @@ function RecordSheet() {
                                 whiteSpace: "nowrap",
                               }}
                             >
-                              <span style={{ fontWeight: 700, color: "#333" }}>{item.label}:</span>{" "}
                               {item.average.toLocaleString()}
                             </td>
                           ))}
